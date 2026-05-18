7ba9dbbaba31046d24a64825054f22c9c581a46d
digitalprasha <digitalprasha@gmail.com>
2026-05-18 23:12:38 +0700

    fix: add CSP headers for eval, fix Vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; object-src 'none';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
